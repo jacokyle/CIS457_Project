c1b9c7632cd0b95a960b276733073aa791ba1c3d
jacokyle <jacokyle@mail.gvsu.edu>
2020-10-19 15:49:16 -0400

    Added ability for client and server to echo.

--- client.py
+++ client.py
@@ -5,3 +5,14 @@
 
 # The client program presents a command line interface with actions for the user.
 
+import socket
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 4000         # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
+
+print('Received', repr(data))
