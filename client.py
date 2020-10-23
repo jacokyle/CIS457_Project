cd30646a0c4dcdc3af15072c55aa3b30a13bdbed
jacokyle <jacokyle@mail.gvsu.edu>
2020-10-23 10:07:07 -0400

    Update to header.

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 # Project 1 - Building an FTP Server
 # CIS 457 - Data Communications
-# Authors: Kyle Jacobson, Logan Jaglowski, Kade O'Laughlin
+# Authors: Kevin Rufino, Kyle Jacobson, Logan Jaglowski, Kade O'Laughlin
 # Date of Submission: October 26, 2020
 
 # The client program presents a command menu with actions for the user.
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 # Project 1 - Building an FTP Server
 # CIS 457 - Data Communications
-# Authors: Kyle Jacobson, Logan Jaglowski, Kade O'Laughlin
+# Authors: Kevin Rufino, Kyle Jacobson, Logan Jaglowski, Kade O'Laughlin
 # Date of Submission: October 26, 2020
 
 # The server program binds to a port and listens to requests from a client.
