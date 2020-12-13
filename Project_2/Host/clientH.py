fe39b1c5a9336a1c26247f95d6fb007aed87b5fa
loganjaglowski <30781051+loganjaglowski@users.noreply.github.com>
2020-12-13 02:20:15 -0500

    File Upload and Connection Works

--- Project_2/Host/clientH.py
+++ Project_2/Host/clientH.py
@@ -16,31 +16,31 @@ class Client:
         self.serverHostname = ''
         self.username = ''
         self.ftp = FTP('')
-
     def clientConnect(self, hostName, Portnumber, username, serverHostname, speed):
         self.hostName = hostName
         self.portNumber = Portnumber
         self.username = username
         self.serverHostname = serverHostname
         self.ftp.connect('', 4488)
-
         try:
             self.ftp.login("", "");
         except:
             return "Error authorizing"
         return "Good"
-
     def updateUsersAndFiles(self, fileUpload):
         filename = "users.txt"
         file = open(filename, 'wb')
         self.ftp.retrbinary('RETR ' + filename, open(filename, 'wb').write)
         file.write((self.username+ " " + self.hostName + "" + self.speed + "\n").encode())
         file.close()
-        filename2 = fileUpload
-        file2 = open(fileUpload, 'w')
-        self.ftp.storbinary('STOR ' + filename2, file2)
-        file2.close()
+        fileSend = open(filename, 'rb+')
+        self.ftp.storbinary('STOR %s' % filename, fileSend)
+        fileSend.close()
 
+        filename2 = fileUpload
+        file2 = open(fileUpload, 'rb+')
+        self.ftp.storbinary('STOR %s' % filename2, file2)
+        file2.close()
     def fetchFile(self, filename):
         file = open(filename, 'w')
         ftp.retrbinary('RETR ' + filename, open(filename, 'w').write)
